002f0f3f2462d95907166cf242e6cfbe1baa6c2c
Rocio Jalifi <jalifirocio@gmail.com>
2021-08-06 11:35:22 +0200

    style(projects page): adds style to projects page

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,16 +2,9 @@
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector("nav");
 
-navToggle.addEventListener("click", () => {
-  navToggle.classList.toggle("nav-open");
-  nav.classList.toggle("nav-open");
-});
-
-const navLinks = document.querySelectorAll(".nav__item");
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    navToggle.classList.remove("nav-open");
-    nav.classList.remove("nav-open");
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navToggle.classList.toggle("nav-open");
+    nav.classList.toggle("nav-open");
   });
-});
+}
