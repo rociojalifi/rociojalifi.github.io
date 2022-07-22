0138c372f8a2d7024d718a795d52482e649076c8
rociojalifi <jalifirocio@gmail.com>
2022-07-22 15:59:38 +0200

    add: dark mode & work in progress

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,17 @@ if (navToggle) {
     nav.classList.toggle("nav-open");
   });
 }
+
+let logo = document.getElementById("logo")
+let icon = document.getElementById("icon");
+  icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")){
+    icon.src = "assets/images/light.png"
+    logo.src = "assets/images/dark-logo.png"
+  } else {
+    icon.src = "assets/images/dark.png"
+    logo.src = "assets/images/logo.png"
+
+  }
+}
